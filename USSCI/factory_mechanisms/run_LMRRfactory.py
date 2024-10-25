a29fdb2cfe523cf2da7c490e74790d488cbd147d
pjsingal <pjs2206@columbia.edu>
2024-10-25 01:07:02 +0000

    ran LMRRfactory

--- USSCI/factory_mechanisms/run_LMRRfactory.py
+++ USSCI/factory_mechanisms/run_LMRRfactory.py
@@ -1,20 +1,20 @@
-from src.LMRRfactory import LMRRfactory
+from LMRRfactory import makeYAML
 
 models = {
-    'Alzueta': 'chemical_mechanisms\\Alzueta-2023\\alzuetamechanism.yaml',
-    'Mei': 'chemical_mechanisms\\Mei-2019\\mei-2019.yaml',
-    # 'Glarborg': "chemical_mechanisms\\Glarborg-2018\\glarborg-2018.yaml",
-    'Zhang': "chemical_mechanisms\\Zhang-2017\\zhang-2017.yaml",
-    'Otomo': "chemical_mechanisms\\Otomo-2018\\otomo-2018.yaml",
-    'Stagni': "chemical_mechanisms\\Stagni-2020\\stagni-2020.yaml",
-    # 'Shrestha': "chemical_mechanisms\\Shrestha-2021\\shrestha-2021.yaml",
-    'Han': "chemical_mechanisms\\Han-2021\\han-2021.yaml"
+    'Alzueta': 'chemical_mechanisms/Alzueta-2023/alzuetamechanism.yaml',
+    'Mei': 'chemical_mechanisms/Mei-2019/mei-2019.yaml',
+    # 'Glarborg': "chemical_mechanisms/Glarborg-2018/glarborg-2018.yaml",
+    'Zhang': "chemical_mechanisms/Zhang-2017/zhang-2017.yaml",
+    'Otomo': "chemical_mechanisms/Otomo-2018/otomo-2018.yaml",
+    'Stagni': "chemical_mechanisms/Stagni-2020/stagni-2020.yaml",
+    # 'Shrestha': "chemical_mechanisms/Shrestha-2021/shrestha-2021.yaml",
+    'Han': "chemical_mechanisms/Han-2021/han-2021.yaml"
     }
 
 for m in models.keys():
-    base = {'mechanism': models[m]}
-    base['colliders'] = 'test\\testinput.yaml'
-    LMRRfactory(baseInput=base,allPdep=False,date='Oct22')
+    makeYAML(mechInput=models[m],
+             outputPath='USSCI/factory_mechanisms',
+             allPdep=False,date='Oct24')
 
 # import numpy as np
 # T_list=np.linspace(200,2000,100)
