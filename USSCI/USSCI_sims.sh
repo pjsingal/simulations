f64027e532c128a70b94af02c145081928ff44bd
pjsingal <pjs2206@columbia.edu>
2024-11-23 21:39:41 +0000

    gen 3 new sets of factory mechs

--- USSCI/USSCI_sims.sh
+++ USSCI/USSCI_sims.sh
@@ -12,63 +12,41 @@ lgdfsz=7
 
 # python startup.py &
 
-date='Nov22'
+date='Nov23'
 
-# python USSCI/runLMRRfactory.py --date $date --allPdep 'True' --allPLOG 'True'
-
-# python USSCI/simulateIDT_Shao_USSCI.py \
-# --figwidth 7 --figheight 6.66667 --fsz $fszaxlab --fszxtick $fszxtick --fszytick $fszytick --fszaxlab $fszaxlab \
-# --lw $lw --mw $mw --msz $msz --lgdw $lgdw --lgdfsz 7 --gridsz 9 --date $date &
-
-# python USSCI/simulateshocktubeAramco_USSCI.py \
-# --figwidth 7 --figheight 2.5 --fsz $fszaxlab --fszxtick $fszxtick --fszytick $fszytick --fszaxlab $fszaxlab \
-# --lw $lw --mw $mw --msz $msz --lgdw $lgdw --lgdfsz 7 --gridsz 3 --date $date &
-
-# python USSCI/simulateJSR_H2O_Alzueta.py \
-# --figwidth 6.5 --figheight 2.5 --fsz $fszxtick --fszxtick $fszxtick --fszytick $fszytick --fszaxlab $fszaxlab \
-# --lw $lw --mw $mw --msz $msz --lgdw $lgdw --lgdfsz 5.8 --gridsz 50 --date $date
-
-# python USSCI/simulateresidencetimeGubbi_USSCI.py --date $date --slopeVal 0.05 --curveVal 0.05
-
-# python USSCI/simulateflamespeedGubbi_vsP_USSCI.py --date $date --slopeVal 0.05 --curveVal 0.05 --gridsz 50
-
-# python USSCI/simulateflamespeedGubbi_vsPhi_USSCI.py --date $date --slopeVal 0.05 --curveVal 0.05 --gridsz 20
-
-# python USSCI/simulateresidencetimeGubbi_FromData.py \
-# --figwidth 6.5 --figheight 2.72 --fsz $fszaxlab --fszxtick $fszxtick --fszytick $fszytick --fszaxlab $fszaxlab \
-# --lw $lw --mw $mw --msz $msz --lgdw $lgdw --lgdfsz $lgdfsz --date $date --slopeVal 0.05 --curveVal 0.05 --xscale 'log' --yscale 'log'&
-
-
-#### JET-STIRRED REACTOR WITH NH3 DILUENT
-# python USSCI/simulateJSR_USSCI.py \
-# --figwidth 6 --figheight 6 --fsz $fszxtick --fszxtick $fszxtick --fszytick $fszytick --fszaxlab $fszaxlab \
-# --lw $lw --mw $mw --msz $msz --lgdw $lgdw --lgdfsz 7 --gridsz 100 --date $date
+python USSCI/runLMRRfactory.py --date $date --allPdep 'True' --allPLOG 'True'
 
 # python USSCI/simulateFR_phi_USSCI.py \
 # --figwidth 6 --figheight 6 --fsz $fszxtick --fszxtick $fszxtick --fszytick $fszytick --fszaxlab $fszaxlab \
 # --lw $lw --mw $mw --msz $msz --lgdw $lgdw --lgdfsz 7 --gridsz 30 --date $date
 
-# python USSCI/simulateJSR_BartokGlarborg.py \
-# --figwidth 6 --figheight 6 --fsz $fszxtick --fszxtick $fszxtick --fszytick $fszytick --fszaxlab $fszaxlab \
-# --lw $lw --mw $mw --msz $msz --lgdw $lgdw --lgdfsz 7 --gridsz 70 --date $date
-
-# python USSCI/simulateJSR_AlzuetaFig3.py \
-# --figwidth 6 --figheight 6 --fsz $fszxtick --fszxtick $fszxtick --fszytick $fszytick --fszaxlab $fszaxlab \
-# --lw $lw --mw $mw --msz $msz --lgdw $lgdw --lgdfsz 7 --gridsz 30 --date $date
-
-
 # python USSCI/simulateST_USSCI.py \
 # --figwidth 6 --figheight 6 --fsz $fszxtick --fszxtick $fszxtick --fszytick $fszytick --fszaxlab $fszaxlab \
 # --lw $lw --mw $mw --msz $msz --lgdw $lgdw --lgdfsz 7 --date $date
-
 
 # python USSCI/simulateIDT_USSCI.py \
 # --figwidth 6 --figheight 6 --fsz $fszxtick --fszxtick $fszxtick --fszytick $fszytick --fszaxlab $fszaxlab \
 # --lw $lw --mw $mw --msz $msz --lgdw $lgdw --lgdfsz 7 --gridsz 20 --date $date
 
-python USSCI/simulateIDT_USSCI_diluent.py \
---figwidth 6 --figheight 6 --fsz $fszxtick --fszxtick $fszxtick --fszytick $fszytick --fszaxlab $fszaxlab \
---lw $lw --mw $mw --msz $msz --lgdw $lgdw --lgdfsz 7 --gridsz 20 --date $date
+# python USSCI/simulateIDT_USSCI_diluent.py \
+# --figwidth 6 --figheight 6 --fsz $fszxtick --fszxtick $fszxtick --fszytick $fszytick --fszaxlab $fszaxlab \
+# --lw $lw --mw $mw --msz $msz --lgdw $lgdw --lgdfsz 7 --gridsz 20 --date $date
+
+# python USSCI/simulateIDT_Beigzadeh.py \
+# --figwidth 6 --figheight 6 --fsz $fszxtick --fszxtick $fszxtick --fszytick $fszytick --fszaxlab $fszaxlab \
+# --lw $lw --mw $mw --msz $msz --lgdw $lgdw --lgdfsz 7 --gridsz 30 --date $date
+
+# python USSCI/simulateIDT_Dai.py \
+# --figwidth 6 --figheight 6 --fsz $fszxtick --fszxtick $fszxtick --fszytick $fszytick --fszaxlab $fszaxlab \
+# --lw $lw --mw $mw --msz $msz --lgdw $lgdw --lgdfsz 7 --gridsz 30 --date $date
+
+# python USSCI/simulateJSR_Lavadera.py \
+# --figwidth 7 --figheight 3 --fsz $fszxtick --fszxtick $fszxtick --fszytick $fszytick --fszaxlab $fszaxlab \
+# --lw $lw --mw $mw --msz $msz --lgdw $lgdw --lgdfsz 7 --gridsz 15 --date $date
+
+# python USSCI/simulateJSR_Manna.py \
+# --figwidth 7 --figheight 3 --fsz $fszxtick --fszxtick $fszxtick --fszytick $fszytick --fszaxlab $fszaxlab \
+# --lw $lw --mw $mw --msz $msz --lgdw $lgdw --lgdfsz 7 --gridsz 15 --date $date
 
 
 
